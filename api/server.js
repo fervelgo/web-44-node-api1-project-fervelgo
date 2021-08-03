537c76031dde749d3cfa724b33558ddb53529566
Fernando Velasco <fervelgo@gmail.com>
2021-08-03 17:34:40 -0500

    added GET for all ujsers

--- api/server.js
+++ api/server.js
@@ -1,3 +1,21 @@
 // BUILD YOUR SERVER HERE
 
+const express = require('express');
+const users = require('./users/model')
+const server = express()
+
+server.use(express.json())
+
+//GET array of users   
+
+server.get('/api/users', (req,res) => {
+    users.find()
+    .then( user => {
+        res.status(200).json(user)
+    })
+    .catch( () => {
+        res.status(500).json({ message: 'error error error'})
+    });
+})
+
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
