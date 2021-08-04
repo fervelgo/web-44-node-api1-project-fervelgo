508dbdc09db065174da66d74f8bcbd8349833ce3
Fernando Velasco <fervelgo@gmail.com>
2021-08-03 19:01:03 -0500

    corrected massive bug

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 // BUILD YOUR SERVER HERE
 
 const express = require('express');
-const users = require('./users/model')
+const Users = require('./users/model')
 const server = express()
 
 server.use(express.json())
@@ -9,12 +9,12 @@ server.use(express.json())
 //GET array of users   
 
 server.get('/api/users', (req,res) => {
-    users.find()
+    Users.find()
     .then( user => {
         res.status(200).json(user)
     })
     .catch( () => {
-        res.status(500).json({ message: 'error error error'})
+        res.status(500).json({ message: "The users information could not be retrieved"})
     });
 })
 
@@ -22,10 +22,10 @@ server.get('/api/users', (req,res) => {
 
 server.get('/api/users/:id', (req, res) => {
     const { id } = req.params
-    users.findById(id)
+    Users.findById(id)
     .then(users => {
         if (!users) {
-            res.status(404).json({ message: 'user not found'})
+            res.status(404).json({ message: "The user with the specified ID does not exist"})
         } else {
             res.status(200).json(users)
         }
@@ -34,50 +34,51 @@ server.get('/api/users/:id', (req, res) => {
 
 // POST Create a new user inside the body of the request
 
-server.post('api/users/',  (req,res) => {
-    const { name, bio } = req.body
-    users.insert({ name, bio })
-    .then( user => {
-        res.status(201).json(user)
+server.post('/api/users',  (req,res) => {
+    const user = req.body
+    Users.insert(user)
+    .then( newUser => {
+        console.log(user)
+        res.status(201).json(newUser)
     })
     .catch( () => {
-        res.status(500).json({ message: 'Failed to add user'})
+        res.status(500).json({ message: "There was an error while saving the user to the database"})
     })
 });
 
 //PUT Edit user 
 
-server.put('api/users/:id', (req, res) => {
+server.put('/api/users/:id', (req, res) => {
     const { id } = req.params
     const { name, bio } = req.body
-    users.update(id, {name, bio})
+    Users.update(id, {name, bio})
     .then(updateUser => {
         if(!updateUser) {
-            res.status(404).json({message: 'user not found'})
+            res.status(404).json({message: "The user with the specified ID does not exist" })
         } else {
             res.json(updateUser)
         }
     })
     .catch(() => {
-        res.status(500).json({message: 'no user updated'})
+        res.status(500).json({message: "The user information could not be modified"})
     })
 })
 
 //DELETE Delete a user
 
-server.delete('api/users/:id', (req,res) => {
+server.delete('/api/users/:id', (req,res) => {
     const { id } = req.params
-    users.remove(id)
+    Users.remove(id)
     .then(deleted => {
         if(!deleted) {
-            res.status(404).json({message: 'could not find user for deleting'})
+            res.status(404).json({message: "The user with the specified ID does not exist"})
     } else {
         res.json(deleted)
     }
 })
 .catch(() =>{
-    res.status(500.json({message: 'error deleting'}))
+    res.status(500).json({message: "The user could not be removed"})
 })
-
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
